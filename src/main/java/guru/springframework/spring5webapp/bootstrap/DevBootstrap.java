d89c8e33014fd1fa4456ba987ea43e2faed68b7a
ibrahim baykal <halilibrahimbaykal@gmail.com>
2018-02-26 22:24:55 +0300

    Add Publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepositoory;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepositoory bookRepositoory;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepositoory bookRepositoory) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepositoory bookRepositoory,
+                        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepositoory = bookRepositoory;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,9 +31,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
 
+        //Test Publisher
+        Publisher publisher= new Publisher();
+        publisher.setName("Baykal Yayın Evi");
+        publisherRepository.save(publisher);
+
         //Tolstoy
         Author Tolstoy = new Author("Lev", "Tolstoy");
-        Book WarAndPeace = new Book("War and Peace","1234","Yordam Yayınları");
+        Book WarAndPeace = new Book("War and Peace","1234",publisher);
         Tolstoy.getBooks().add(WarAndPeace);
         WarAndPeace.getAuthors().add(Tolstoy);
 
@@ -38,7 +48,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         //Dostoyevsky
         Author Dostoyevsky = new Author("Fyodor","Dostoyevsky");
         Book CrimeAndPunishment = new Book("Crime And Punishment",
-                "1235","Türkiye İş Bankası Kültür Yayınları");
+                "1235",publisher);
         Dostoyevsky.getBooks().add(CrimeAndPunishment);
         CrimeAndPunishment.getAuthors().add(Dostoyevsky);
 
